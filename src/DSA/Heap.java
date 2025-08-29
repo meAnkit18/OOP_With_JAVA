34cf2f90fbdb7f156a5f11de1b1b61461e3e4b93
Ankit Kumar <ankit1842kumar@gmail.com>
2025-08-29 20:00:04 +0530

    8/29/25

---
+++ src/DSA/Heap.java
@@ -0,0 +1,21 @@
+package DSA;
+import java.util.*;
+//final it is mi
+// final stop overring
+public class Heap {
+private int y;
+protected int x;
+public int z;
+int my() {
+	y=y+10;
+	return y;
+}
+	public static void main(String args[]) {
+		Heap s = new Heap();
+		int i = my();
+		System.out.println(i);
+	}
+}
+class student{
+	
+}
